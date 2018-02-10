214d3f3fc7644b44e90b65876dc8d6cbd794b808
sebastian correa <scorrea1@wsgc.com>
2018-02-10 11:18:32 -0500

    Create Stream Behaviors to Push Props in React Components with mapPropsStream

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
 import React, { Component, cloneElement, Children } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { setObservableConfig, componentFromStream, createEventHandler } from "recompose";
+import { setObservableConfig, mapPropsStream, createEventHandler } from "recompose";
 import rxjsConfig from "recompose/rxjsObservableConfig";
 import { Observable } from "rxjs/Observable";
 
 
 setObservableConfig(rxjsConfig)
 
-const Counter = ({ value, onInc, onDec }) => (
-  <div>
-    <button onClick={onInc}>+</button>
-    <h2>{value}</h2>
-    <button onClick={onDec}>-</button>
-  </div>
+const Counter = ({ count }) => (
+    <h2>{count}</h2>
 )
 
 
-const CounterStream = componentFromStream(props$ => {
-  const { stream: onInc$, handler: onInc } = createEventHandler()
-  const { stream: onDec$, handler: onDec } = createEventHandler()
-
+const interval = mapPropsStream(props$ => {
   return props$
     .switchMap(props =>
-      Observable.merge(
-        onInc$.mapTo(1),
-        onDec$.mapTo(-1)
-      )
-        .startWith(props.value)
-        .scan((acc, curr) => acc + curr)
-        .map(value => ({ value, onInc, onDec }))
-        .map(newProps =>
-          Children.map(props.children, 
-            child => 
-            cloneElement(child, newProps)
-          )
-        )
+      Observable.interval(1000),
+      (props, count) => ({ ...props, count })
     )
 })
 
+const WithInterval = interval(Counter)
+
 const App = () => (
- <CounterStream value={3}>
-    <Counter />
-    <Counter />
- </CounterStream>
+ <WithInterval />
 )
 
 export default App;
