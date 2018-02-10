88de2a1da07fd19a5a7174e7f6a36a84205f77c1
sebastian correa <scorrea1@wsgc.com>
2018-02-10 12:37:20 -0500

    Compose Streams of React Props with Recompose’s compose and RxJS

--- src/App.js
+++ src/App.js
@@ -1,30 +1,82 @@
 import React, { Component, cloneElement, Children } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { setObservableConfig, mapPropsStream, createEventHandler } from "recompose";
+import {
+  setObservableConfig,
+  mapPropsStream,
+  createEventHandler,
+  compose
+} from "recompose";
 import rxjsConfig from "recompose/rxjsObservableConfig";
 import { Observable } from "rxjs/Observable";
 
 
 setObservableConfig(rxjsConfig)
 
-const Counter = ({ count }) => (
-    <h2>{count}</h2>
+const Counter = props => (
+  <div>
+    <button onClick={props.onInc}>+</button>
+    <button onClick={props.onDec}>-</button>
+    <h3>{props.count}</h3>
+    <h1>{props.person.name}</h1>
+  </div>
 )
 
+const count = mapPropsStream(props$ => {
+  const { stream: onInc$, handler: onInc } = createEventHandler()
+  const { stream: onDec$, handler: onDec } = createEventHandler()
 
-const interval = mapPropsStream(props$ => {
   return props$
     .switchMap(props =>
-      Observable.interval(1000),
-      (props, count) => ({ ...props, count })
+      Observable.merge(
+        onInc$.mapTo(1),
+        onDec$.mapTo(-1)
+      )
+      .startWith(0)
+      .scan((acc, curr) => acc + curr),
+      (props, count) => ({
+        ...props,
+        count,
+        onInc,
+        onDec
+      })
     )
 })
 
-const WithInterval = interval(Counter)
+
+const load = mapPropsStream(props$ =>
+  props$
+  .switchMap( props =>
+      Observable.ajax(
+        `https://swapi.co/api/people/${props.count}`
+      )
+      .pluck("response")
+      .startWith({ name: "loading..." })
+      .catch(err =>
+        Observable.of({ name: "Not found" })
+      ),
+    (props, person) => ({ ...props, person })
+  )
+)
+
+const type = mapPropsStream(props$ =>
+  props$.
+    switchMap(props =>
+      Observable.zip(
+        Observable.from(props.person.name),
+        Observable.interval(300),
+        letter => letter
+      )
+        .scan((acc, curr) => acc + curr),
+    (props, name) => ({...props, person:{ ...props.person, name }})
+  )
+)
+
+const CounterWithPersonLoader = compose(count, load, type)(Counter)
+
 
 const App = () => (
- <WithInterval />
+ <CounterWithPersonLoader  />
 )
 
 export default App;
