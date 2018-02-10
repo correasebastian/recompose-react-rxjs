1107b7a0f712c9a668d270528608a9e8fba31c21
sebastian correa <scorrea1@wsgc.com>
2018-02-09 22:56:26 -0500

    Handle React Events as Streams with RxJS and Recompose

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { setObservableConfig, componentFromStream } from "recompose";
+import { setObservableConfig, componentFromStream, createEventHandler } from "recompose";
 import rxjsConfig from "recompose/rxjsObservableConfig";
 import { Observable } from "rxjs/Observable";
 
 
 setObservableConfig(rxjsConfig)
+const loginput = e => console.log(e.target.value);
 
-const personById = id => `https://swapi.co/api/people/${id}`
-
-const loadById = id =>
-  Observable.ajax(personById(id))
-    .pluck('response')
-    .switchMap(res =>
-      Observable.ajax(res.homeworld)
-        .pluck('response')
-        .startWith({name:'seb'}),
-      (person, homeworld) => ({
-        ...person,
-        homeworld: homeworld.name
-      })
-    )
-
-const Card = props => (
+const SimpleForm = ({ text, onInput }) => (
   <div>
-    <h1>{props.name}</h1>
-    <h2>{props.homeworld}</h2>
+    <input type="text" onInput={onInput} />
+    <h2>{text}</h2>
   </div>
 )
 
+const SimpleFormStream =
+  componentFromStream(props$ => {
+    const { stream: onInput$, handler: onInput } = createEventHandler();
 
-const CardStream = componentFromStream(
-  props$ =>
-    props$
-      .switchMap(props =>
-        loadById(props.id)
-      )
-      .map(Card)
+    const text$ = onInput$
+      .map(e => e.target.value)
+      .delay(500)
+      .startWith('')
+
+    return text$
+      .map(text => ({ text, onInput }))
+      .map(SimpleForm)
+
+  })
+
+
+const App = () => (
+ <SimpleFormStream />
 )
 
-
-export default CardStream;
+export default App;
