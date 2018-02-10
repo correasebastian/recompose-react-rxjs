aa5f7e2ee9881c628c9cce1366acee759c570598
sebastian correa <scorrea1@wsgc.com>
2018-02-09 20:42:59 -0500

    with Observable.interval

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import { setObservableConfig, componentFromStream } from "recompose";
 import rxjsConfig from "recompose/rxjsObservableConfig";
+import { Observable } from "rxjs/Observable";
 
 
 setObservableConfig(rxjsConfig)
 
 const App = componentFromStream(
-  props$ =>  props$.map(prop => (<h1>Hi</h1> ))
+  props$ =>  Observable.interval(1000).map(count => (<h1>{count}</h1> ))
 )
 
 export default App;
